f9739dd77b2fa59e29515d2ac52bd04137effb8b
enoa153974 <aoi.153974@gmail.com>
2025-08-13 16:57:48 +0900

    ogpimage追加

--- siteData.mjs
+++ siteData.mjs
@@ -6,42 +6,42 @@ export default {
     home: {
         title: 'machikado cafe | 香里園にあるクレープとコーヒーのお店です',
         description: 'machikado cafeは大阪府寝屋川市香里園アルプラザのフードコート1階にあるこだわりのクレープとコーヒーのお店です。',
-        ogpImage: '/assets/images/ogp_default.png',
+        ogpImage: '/assets/images/tentou_01.jpg',
         type: 'website',
         path: '/'
     },
     info: {
         title: '店舗情報 | machikadocafe',
         description: 'machikado cafeは大阪府寝屋川市香里園アルプラザのフードコート1階にあるこだわりのクレープとコーヒーのお店です。',
-        ogpImage: '/assets/images/ogp_default.png',
+        ogpImage: '/assets/images/tentou_01.jpg',
         type: 'article',
         path: '/info'
     },
         contact: {
             title: 'お問い合わせ | machikado cafe',
             description: 'お問い合わせやご相談は下記フォームからお気軽にどうぞ。',
-            ogpImage: '/assets/images/ogp_contact.png',
+            ogpImage: '/assets/images/tentou_01.jpg',
             type: 'article',
             path: '/info/contact'
         },
     intro: {
         title: 'お店の紹介 | machikadocafe',
         description: 'machikado cafeは大阪府寝屋川市香里園アルプラザのフードコート1階にあるこだわりのクレープとコーヒーのお店です。',
-        ogpImage: '/assets/images/ogp_default.png',
+        ogpImage: '/assets/images/tentou_01.jpg',
         type: 'article',
         path: '/intro'
     },
     menu: {
         title: 'クレープメニュー | machikadocafe',
         description: 'machikado cafeは大阪府寝屋川市香里園アルプラザのフードコート1階にあるこだわりのクレープとコーヒーのお店です。',
-        ogpImage: '/assets/images/ogp_default.png',
+        ogpImage: '/assets/images/tentou_01.jpg',
         type: 'article',
         path: '/menu'
     },
     news: {
         title: 'お知らせ一覧 | machikadocafe',
         description: 'machikado cafeは大阪府寝屋川市香里園アルプラザのフードコート1階にあるこだわりのクレープとコーヒーのお店です。',
-        ogpImage: '/assets/images/ogp_default.png',
+        ogpImage: '/assets/images/tentou_01.jpg',
         type: 'article',
         path: '/news'
     }
